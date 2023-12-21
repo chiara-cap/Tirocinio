cdfd3586d86577056b0ef6c83174bd5a77cc0dc6
chiara-cap <255264@studenti.unimore.it>
2023-12-21 19:07:40 +0100

    Notebook Jupyter experiments

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 . /usr/local/anaconda3/etc/profile.d/conda.sh 
 conda activate clip
 
-srun -Q --immediate=10 --partition=all_serial --gres=gpu:1 --time 2:00:00 --pty bash
+srun -Q --immediate=10 --partition=all_serial --gres=gpu:1 --time 10:00 --pty bash
 
